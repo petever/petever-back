9844251f71ccdd1b47123c8ca47f9f53c9c25f53
rumor1993 <rumor1993@naver.com>
2022-12-28 23:34:40 +0900

    feature: cors open

--- src/main/java/com/example/petever/domain/board/web/BoardController.java
+++ src/main/java/com/example/petever/domain/board/web/BoardController.java
@@ -3,16 +3,14 @@ package com.example.petever.domain.board.web;
 import com.example.petever.domain.board.application.BoardService;
 import com.example.petever.domain.board.web.response.BoardSummary;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/boards")
 @RequiredArgsConstructor
+@CrossOrigin(value = "*")
 public class BoardController {
 
     private final BoardService boardService;
