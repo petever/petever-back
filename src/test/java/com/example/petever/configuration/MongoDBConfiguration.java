a72af1c1dd2b7de24a60c7358c9224ef26a85c30
mark <mark@101.inc>
2023-01-13 00:41:41 +0900

    feat. embedded mongo를 통해 mongoDB 테스트를 설정한다

--- src/test/java/com/example/petever/configuration/MongoDBConfiguration.java
+++ src/test/java/com/example/petever/configuration/MongoDBConfiguration.java
@@ -1,12 +1,13 @@
 package com.example.petever.configuration;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Import;
 
 @TestConfiguration
-@EnableAutoConfiguration(exclude = EmbeddedMongoAutoConfiguration.class)
+@EnableAutoConfiguration(exclude = {EmbeddedMongoAutoConfiguration.class, MongoAutoConfiguration.class})
 @Import(de.flapdoodle.embed.mongo.spring.autoconfigure.EmbeddedMongoAutoConfiguration.class)
 public class MongoDBConfiguration {
 }
