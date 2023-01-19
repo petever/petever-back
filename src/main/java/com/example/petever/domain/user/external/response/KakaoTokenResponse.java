01eb547f0c5409137bcbf2d34f21841367e3ca81
rumor1993 <rumor1993@naver.com>
2023-01-20 02:43:35 +0900

    feature: JsonCreator KakaoTokenResponse

--- src/main/java/com/example/petever/domain/user/external/response/KakaoTokenResponse.java
+++ src/main/java/com/example/petever/domain/user/external/response/KakaoTokenResponse.java
@@ -1,10 +1,11 @@
 package com.example.petever.domain.user.external.response;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public class KakaoTokenResponse {
     private final String accessToken;
     private final String expiresIn;
@@ -13,4 +14,29 @@ public class KakaoTokenResponse {
     private final String scope;
     private final String tokenType;
     private final String idToken;
+
+    @JsonCreator
+    public KakaoTokenResponse(
+            @JsonProperty("access_token")
+            String accessToken,
+            @JsonProperty("expires_in")
+            String expiresIn,
+            @JsonProperty("refresh_token")
+            String refreshToken,
+            @JsonProperty("refresh_token_expires_in")
+            String refreshTokenExpiresIn,
+            @JsonProperty("scope")
+            String scope,
+            @JsonProperty("token_type")
+            String tokenType,
+            @JsonProperty("id_token")
+            String idToken) {
+        this.accessToken = accessToken;
+        this.expiresIn = expiresIn;
+        this.refreshToken = refreshToken;
+        this.refreshTokenExpiresIn = refreshTokenExpiresIn;
+        this.scope = scope;
+        this.tokenType = tokenType;
+        this.idToken = idToken;
+    }
 }
