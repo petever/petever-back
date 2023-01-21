7da297251ea8b5daa130611f255528225752e83a
mark <mark@101.inc>
2023-01-21 18:00:51 +0900

    feat. image 를 저장하고 불러올 수 있도록 Controller 를 구현한다

--- src/main/java/com/example/petever/domain/post/service/EC2FileSaveService.java
+++ src/main/java/com/example/petever/domain/post/service/EC2FileSaveService.java
@@ -7,6 +7,11 @@ import com.example.petever.utils.FileUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.Map;
 
 @Component
@@ -18,7 +23,29 @@ public class EC2FileSaveService implements RawImageSaveService {
     @Override
     public Image save(RawImage rawImage) {
         byte[] images = rawImage.getImage();
-        RawImage thumbnailImage = rawImage.thumbnail(300, 300);
+        int thumbnailWidth = 0;
+        int thumbnailHeight = 0;
+        try (ByteArrayInputStream stream = new ByteArrayInputStream(rawImage.getImage())) {
+            BufferedImage image = ImageIO.read(stream);
+
+            BigDecimal width = BigDecimal.valueOf(image.getWidth());
+            BigDecimal height = BigDecimal.valueOf(image.getHeight());
+
+            if (width.compareTo(new BigDecimal("700")) <= 0) {
+                thumbnailWidth = width.intValue();
+            } else {
+                thumbnailWidth = width.multiply(BigDecimal.valueOf(0.5)).intValue();
+            }
+
+            if (width.compareTo(new BigDecimal("700")) <= 0) {
+                thumbnailHeight = height.intValue();
+            } else {
+                thumbnailHeight = height.multiply(BigDecimal.valueOf(0.5)).intValue();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Thumbnail Error", e);
+        }
+        RawImage thumbnailImage = rawImage.thumbnail(thumbnailWidth, thumbnailHeight);
         String originImagePath = FileUtil.save(images, rawImage.getFileName(), rawImage.getExt(), fileConfiguration);
         String thumbnailImagePath = FileUtil.save(thumbnailImage.getImage(), thumbnailImage.getFileName(), thumbnailImage.getExt(), fileConfiguration);
         return new Image(
