8f39b25d1cdbea283f8fd1e254e3fe9f2818429b
rumor1993 <rumor1993@naver.com>
2023-02-03 01:35:59 +0900

    feature: add CrossOrigin

--- src/main/java/com/example/petever/domain/user/web/UserController.java
+++ src/main/java/com/example/petever/domain/user/web/UserController.java
@@ -16,7 +16,7 @@ import java.io.IOException;
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "https://petever.pet")
+@CrossOrigin(origins = "*")
 public class UserController {
 
     private final UserSessionService sessionService;
