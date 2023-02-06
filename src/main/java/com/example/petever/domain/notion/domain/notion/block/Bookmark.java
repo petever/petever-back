3f361dae14cd51a9d9e3947f26561e24247af358
rumor1993 <rumor1993@naver.com>
2023-02-06 22:54:53 +0900

    feature: add json property

--- src/main/java/com/example/petever/domain/notion/domain/notion/block/Bookmark.java
+++ src/main/java/com/example/petever/domain/notion/domain/notion/block/Bookmark.java
@@ -1,13 +1,19 @@
 package com.example.petever.domain.notion.domain.notion.block;
 
-import lombok.RequiredArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.ArrayList;
 
-@RequiredArgsConstructor
 public class Bookmark {
     private final ArrayList<String> caption;
     private final String url;
+
+    public Bookmark(
+            @JsonProperty("caption") ArrayList<String> caption,
+            @JsonProperty("url") String url) {
+        this.caption = caption;
+        this.url = url;
+    }
 
     public String getUrl() {
         if (url == null) {
